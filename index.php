bac8f7e8ca2185efda6efda026a400e209692430
Carl Reed <carl.w.reed@vumc.org>
2018-12-06 17:33:27 -0600

    boilerplate updates

--- index.php
+++ index.php
@@ -10,14 +10,16 @@
 require_once "../../redcap_connect.php";
 
 # get base HTML and substitute file paths to css and js files
-$html = file_get_contents(dirname(__FILE__) . "\html\base.html");
-$html = str_replace("{STYLESHEET}", dirname(__FILE__) . "\css\base.css", $html);
-$html = str_replace("{JQUERY}", dirname(__FILE__) . "\js\jquery-3.3.1.min.js", $html);
-$html = str_replace("{POPPER}", dirname(__FILE__) . "\js\popper.min.js", $html);
-$html = str_replace("{BOOTSTRAP}", dirname(__FILE__) . "\js\bootstrap.min.js", $html);
-$html = str_replace("{JAVASCRIPT}", dirname(__FILE__) . "\js\base.js", $html);
+$html = file_get_contents("html/base.html");
+$html = str_replace("{STYLESHEET}", "css/base.css", $html);
+$html = str_replace("{JQUERY}", "js/jquery-3.3.1.slim.min.js", $html);
+$html = str_replace("{POPPER}", "js/popper.min.js", $html);
+$html = str_replace("{BOOTSTRAP}", "js/bootstrap.min.js", $html);
+$html = str_replace("{JAVASCRIPT}", "js/base.js", $html);
 
-$html = str_replace("{TITLE}", "Plugin Template", $html);
-$html = str_replace("{BODY}", "<h3>TEMPLATE</h3>", $html);
+$html = str_replace("{TITLE}", "ARC Trial - Nitin Dashboards", $html);
+
+$body = $html = file_get_contents("html/one.html");
+$html = str_replace("{BODY}", $body, $html);
 
 echo $html;
