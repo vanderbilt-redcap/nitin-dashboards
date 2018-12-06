071462dc3b5ebea6ed70e9ddd86bad909b4f3b58
Carl Reed <carl.w.reed@vumc.org>
2018-12-06 12:33:27 -0600

    added scss from bootstrap, changed from CDN to local filepaths

--- index.php
+++ index.php
@@ -9,10 +9,15 @@
 // Call the REDCap Connect file in the main "redcap" directory
 require_once "../../redcap_connect.php";
 
+# get base HTML and substitute file paths to css and js files
 $html = file_get_contents(dirname(__FILE__) . "\html\base.html");
 $html = str_replace("{STYLESHEET}", dirname(__FILE__) . "\css\base.css", $html);
+$html = str_replace("{JQUERY}", dirname(__FILE__) . "\js\jquery-3.3.1.min.js", $html);
+$html = str_replace("{POPPER}", dirname(__FILE__) . "\js\popper.min.js", $html);
+$html = str_replace("{BOOTSTRAP}", dirname(__FILE__) . "\js\bootstrap.min.js", $html);
 $html = str_replace("{JAVASCRIPT}", dirname(__FILE__) . "\js\base.js", $html);
-$html = str_replace("{TITLE}", "Base HTML", $html);
-$html = str_replace("{BODY}", "<h3>BASE</h3>", $html);
+
+$html = str_replace("{TITLE}", "Plugin Template", $html);
+$html = str_replace("{BODY}", "<h3>TEMPLATE</h3>", $html);
 
 echo $html;
