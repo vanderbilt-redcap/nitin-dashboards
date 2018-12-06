8802b1e7657fe2bbe510c71f554250fb366c301f
Carl Reed <carl.w.reed@vumc.org>
2018-12-06 10:42:29 -0600

    initial git repo creation

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * PLUGIN NAME: Nitin Dashboard
+ * DESCRIPTION: Shows reports on ARC trial data
+ * VERSION: 1.0
+ * AUTHOR: carl.w.reed@vumc.org
+ */
+
+// Call the REDCap Connect file in the main "redcap" directory
+require_once "../../redcap_connect.php";
+
