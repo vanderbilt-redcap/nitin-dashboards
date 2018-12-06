2da0932c469abc75aa06db834d5050e5f403d5a7
Carl Reed <carl.w.reed@vumc.org>
2018-12-06 10:54:45 -0600

    added base plugin files (html, css, js)

--- index.php
+++ index.php
@@ -9,3 +9,10 @@
 // Call the REDCap Connect file in the main "redcap" directory
 require_once "../../redcap_connect.php";
 
+$html = file_get_contents(dirname(__FILE__) . "\html\base.html");
+$html = str_replace("{STYLESHEET}", dirname(__FILE__) . "\css\base.css", $html);
+$html = str_replace("{JAVASCRIPT}", dirname(__FILE__) . "\js\base.js", $html);
+$html = str_replace("{TITLE}", "Base HTML", $html);
+$html = str_replace("{BODY}", "<h3>BASE</h3>", $html);
+
+echo $html;
