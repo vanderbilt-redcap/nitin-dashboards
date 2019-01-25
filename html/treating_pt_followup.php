d11f09f16e4f4fdca474738003577cd13eaeeb20
Carl Reed <carl.w.reed@vumc.org>
2019-01-25 14:50:34 -0600

    added labelizeValue, corrected logic, reduce getData calls

--- html/treating_pt_followup.php
+++ html/treating_pt_followup.php
@@ -2,127 +2,133 @@
 
 global $dash;
 $content = "";
+$params = [
+	"project_id" => $dash->pid,
+	"return_format" => 'array',
+	"exportDataAccessGroups" => true
+];
+$records = \REDCap::getData($params);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// UNVERIFIED
-// # add Treating Physical Therapist Initial Info table
 $table = [
 	"title" => "Initial Treating PT Referral/Notification Needed",
 	"titleClass" => "blueHeader",
 	"headers" => ["Study ID", "DAG", "Patient Info Release Sent?", "Site lead PT treating PT?"],
-	"content" => [
-		// ["4-3285-119", "", "", "", ""],
-		// ["10-2284-23", "", "", "", ""]
-	]
+	"content" => []
 ];
-$params = [
-	"project_id" => $dash->pid,
-	"return_format" => 'array',
-	"filterLogic" => "([baseline_arm_1][qtk_physical_therapy] = '1') AND ((([enrollment_arm_1][pti_lead_pt_is_treating_pt]= '0' AND [enrollment_arm_1][pti_contact_pt] <> '1') OR ([enrollment_arm_1][pti_lead_pt_is_treating_pt]= '1' AND [enrollment_arm_1][pti_patient_is_participant] <> '1')) OR ([enrollment_arm_1][pti_lead_pt_is_treating_pt]= ''))",
-	"exportDataAccessGroups" => true
-];
-$records = \REDCap::getData($params);
-// foreach ($records as $i => $record) {
-	// foreach ($record as $eid => $data) {
-		// if ($record[$baseline]) {
-			// $row = [];
-			// $row[0] = $record[$dash->enrollmentEID]['study_id'];
-			// $row[1] = $record[$dash->enrollmentEID]['pati_6'];
-			
-			// # manually format this value
-			// $leadPT = $record[$dash->enrollmentEID]['pti_lead_pt_is_treating_pt'];
-			// $row[2] = $leadPT == '0' ? 'No (0)' : ($leadPT == '1' ? 'Yes (1)' : $leadPT);
-			
-			// $row[3] = $record[$dash->enrollmentEID]['pti_lpt_referral_date'];
-			// $row[4] = $record[$dash->enrollmentEID]['pti_pt_contacted'];
-			
-			// $table['content'][] = $row;
-		// }
-	// }
-// }
 foreach ($records as $i => $record) {
-	$row = [];
-	$row[0] = $record[$dash->enrollmentEID]['study_id'];
-	$row[1] = $record[$dash->enrollmentEID]['pati_6'];
-	$row[2] = $record[$dash->enrollmentEID]['pti_release_sent_to_pt'];
-	$row[3] = $record[$dash->enrollmentEID]['pti_lead_pt_is_treating_pt'];
-	
-	$table['content'][] = $row;
-}
-$content .= $dash->makeDataTable($table);
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// # add Outstanding Lead PT Calls table
-$table = [
-	"title" => "Outstanding Lead PT Initial Calls/Verification",
-	"titleClass" => "redHeader",
-	"headers" => ["Study ID", "DAG", "Site Lead PT Treating PT?", "Date referred to lead PT", "Treating Physical therapist successfully contacted?"],
-	"content" => [
-		// ["4-3285-119", "", ""],
-		// ["10-2284-23", "", ""]
-	]
-];
-$params = [
-	"project_id" => $dash->pid,
-	"return_format" => 'array',
-	// "filterLogic" => "(([enrollment_arm_1][pti_patient_is_participant]='1') AND ([enrollment_arm_1][pti_receipt_of_protocol] = '')) OR (([enrollment_arm_1][pti_contact_pt] = '1') AND ([enrollment_arm_1][pti_pt_contacted] = ''))",
-	"exportDataAccessGroups" => true
-];
-$records = \REDCap::getData($params);
-foreach ($records as $i => $record) {
-	foreach ($record as $eid => $data) {
-		if (($data['pti_patient_is_participant'] == '1' and $data['pti_receipt_of_protocol'] == '') or ($data['pti_contact_pt'] == '1' and $data['pti_pt_contacted'] == '')) {
-			$row = [];
-			$row[0] = $record[$dash->enrollmentEID]['study_id'];
-			$row[1] = $record[$dash->enrollmentEID]['pati_6'];
-			
-			# manually format this value
-			$leadPT = $record[$dash->enrollmentEID]['pti_lead_pt_is_treating_pt'];
-			$row[2] = $leadPT == '0' ? 'No (0)' : ($leadPT == '1' ? 'Yes (1)' : $leadPT);
-			
-			$row[3] = $record[$dash->enrollmentEID]['pti_lpt_referral_date'];
-			$row[4] = $record[$dash->enrollmentEID]['pti_pt_contacted'];
-			
-			$table['content'][] = $row;
-		}
+	$bdata = $record[$dash->baselineEID];
+	$edata = $record[$dash->enrollmentEID];
+	if(
+	$bdata['qtk_physical_therapy'] == '1' and
+	((($edata['pti_lead_pt_is_treating_pt'] == '0' and
+	$edata['pti_contact_pt'] <> '1') or
+	($edata['pti_lead_pt_is_treating_pt'] == '1' and
+	$edata['pti_patient_is_participant'] <> '1')) or
+	$edata['pti_lead_pt_is_treating_pt'] == '')
+	) {
+		$row = [];
+		$row[0] = $edata['study_id'];
+		$row[1] = $edata['pati_6'];
+		$row[2] = $dash->labelizeValue('pti_release_sent_to_pt', $edata['pti_release_sent_to_pt']);
+		
+		$val = $edata['pti_lead_pt_is_treating_pt'];
+		$row[3] = $val == '1' ? "Yes (1)" : ($val == '0' ? "No (0)" : $val);
+		
+		$table['content'][] = $row;
 	}
 }
 $content .= $dash->makeDataTable($table);
 
-// UNVERIFIED
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$table = [
+	"title" => "Outstanding Lead PT Initial Calls/Verification",
+	"titleClass" => "redHeader",
+	"headers" => ["Study ID", "DAG", "Site Lead PT Treating PT?", "Date referred to lead PT", "Treating Physical therapist successfully contacted?"],
+	"content" => []
+];
+foreach ($records as $i => $record) {
+	$edata = $record[$dash->enrollmentEID];
+	if (
+	($edata['pti_patient_is_participant'] == '1' and
+	$edata['pti_receipt_of_protocol'] == '') or
+	($edata['pti_contact_pt'] == '1' and 
+	$edata['pti_pt_contacted'] == '')
+	) {
+		$row = [];
+		$row[0] = $edata['study_id'];
+		$row[1] = $edata['pati_6'];
+		
+		# manually format this value
+		$leadPT = $edata['pti_lead_pt_is_treating_pt'];
+		$row[2] = $leadPT == '0' ? 'No (0)' : ($leadPT == '1' ? 'Yes (1)' : $leadPT);
+		
+		$row[3] = $edata['pti_lpt_referral_date'];
+		$row[4] = $edata['pti_pt_contacted'];
+		
+		$table['content'][] = $row;
+	}
+}
+$content .= $dash->makeDataTable($table);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // # add PT Reports to send table
 $table = [
 	"title" => "PT Reports to send",
 	"titleClass" => "blueHeader",
 	"headers" => ["Study ID", "DAG", "Randomization Group", "Event", "Lower Window", "Ideal Date"],
-	"content" => [
-		// ["4-3285-119", "1-month", "10/30/18", "10/22/18"],
-		// ["10-2284-23", "3-month", "10/30/18", ""]
-	]
+	"content" => []
 ];
-$params = [
-	"project_id" => $dash->pid,
-	"return_format" => 'array',
-	"filterLogic" => "((([1month_arm_1][pttk_pt_report_sent] = '' OR [3months_arm_1][pttk_pt_report_sent] = '') AND ([enrollment_arm_1][pati_study_status]<>'0') AND ([1month_arm_1][pttk_pt_report_sent] = '' OR [3months_arm_1][pttk_pt_report_sent] = '')) AND ((([1month_arm_1][pttk_ideal_date_2] <= '2019-01-31' OR [3months_arm_1][pttk_ideal_date_2] <= '2019-01-31') AND ([enrollment_arm_1][randgroup] = '1') AND ([enrollment_arm_1][pati_x15] <> '')) OR (([1month_arm_1][pttk_ideal_date] <= '2019-01-31' OR [3months_arm_1][pttk_ideal_date] <= '2019-01-31') AND ([enrollment_arm_1][randgroup]= '2'))))",
-	"exportDataAccessGroups" => true
-];
-$records = \REDCap::getData($params);
 foreach ($records as $i => $record) {
+	// $date1 = "2019-01-31";
+	$date1 = date('Y-m-d', strtotime('+30 days'));	# set target date to be 30 days from now
+	$edata = $record[$dash->enrollmentEID];
+	$m1data = $record[$dash->m1EID];
+	$m3data = $record[$dash->m3EID];
+	
+		// $row = [];
+		// $row[0] = $edata['study_id'];
+		// $row[1] = $edata['pati_6'];
+		
+		// $rgroup = $edata['randgroup'];
+		// $row[2] = $this->labelizeValue("randgroup", $rgroup);
+		
+		// $row[3] = $dash->projEvents[$eid];
+		// $row[4] = $rgroup == '1' ? $data['pttk_lower_window_2'] : ($rgroup == '2' ? $data['pttk_lower_window'] : '');
+		// $row[5] = $rgroup == '1' ? $data['pttk_ideal_date_2'] : ($rgroup == '2' ? $data['pttk_ideal_date'] : '');
+		
+		// $table['content'][] = $row;
+	
 	foreach ($record as $eid => $data) {
-		$row = [];
-		$row[0] = $record[$dash->enrollmentEID]['study_id'];
-		$row[1] = $record[$dash->enrollmentEID]['pati_6'];
-		
-		# formatting
-		$rgroup = $record[$dash->enrollmentEID]['randgroup'];
-		$row[2] = $rgroup == '1' ? 'Operative (1)' : ($rgroup == 2 ? 'Non-operative (2)' : $rgroup);
-		
-		$row[3] = $dash->projEvents[$eid];
-		$row[4] = $rgroup == '1' ? $data['pttk_lower_window_2'] : ($rgroup == 2 ? $data['pttk_lower_window'] : '');
-		$row[5] = $rgroup == '1' ? $data['pttk_ideal_date_2'] : ($rgroup == 2 ? $data['pttk_ideal_date'] : '');
-		
-		$table['content'][] = $row;
+		if (
+		((($data['pttk_pt_report_sent'] == '' OR
+		$data['pttk_pt_report_sent'] == '') AND
+		($edata['pati_study_status'] <> '0') AND
+		($data['pttk_pt_report_sent'] == '' OR
+		$data['pttk_pt_report_sent'] == '')) AND
+		((($data['pttk_ideal_date_2'] <= $date1 OR
+		$data['pttk_ideal_date_2'] <= $date1) AND
+		($edata['randgroup'] == '1') AND
+		($edata['pati_x15'] <> '')) OR
+		(($data['pttk_ideal_date'] <= $date1 OR
+		$data['pttk_ideal_date'] <= $date1) AND
+		($edata['randgroup'] == '2'))))
+		) {
+			$row = [];
+			$row[0] = $edata['study_id'];
+			$row[1] = $edata['pati_6'];
+			
+			# formatting
+			$rgroup = $edata['randgroup'];
+			// $row[2] = $rgroup == '1' ? 'Operative (1)' : ($rgroup == 2 ? 'Non-operative (2)' : $rgroup);
+			$row[2] = $this->labelizeValue("randgroup", $rgroup);
+			
+			$row[3] = $dash->projEvents[$eid];
+			$row[4] = $rgroup == '1' ? $data['pttk_lower_window_2'] : ($rgroup == '2' ? $data['pttk_lower_window'] : '');
+			$row[5] = $rgroup == '1' ? $data['pttk_ideal_date_2'] : ($rgroup == '2' ? $data['pttk_ideal_date'] : '');
+			
+			$table['content'][] = $row;
+		}
 	}
 }
 $content .= $dash->makeDataTable($table);
--- html/treatment_initiation.php
+++ html/treatment_initiation.php
@@ -3,41 +3,45 @@
 global $dash;
 $content = "";
 
+$params = [
+	"project_id" => $dash->pid,
+	"return_format" => 'array',
+	// "exportAsLabels" => true,
+	// "records" => ["4", "5"],
+	"exportDataAccessGroups" => true
+];
+$records = \REDCap::getData($params);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # add Physical Therapy Scheduling table
 $table = [
 	"title" => "Physical Therapy Scheduling",
 	"titleClass" => "blueHeader",
-	"headers" => ["Study ID", "DAG", "Randomization Group", "Randomization/Surgery Date", "Call 1", "Call 2", "Call 3", "Site referral"],
-	"content" => [
-		# mockup data for visuals
-		// ["4-3285-119", "", "", "8/30/18", "9/10/18", "9/17/18", "9/21/18", "9/25/18"],
-		// ["10-2284-23", "", "", "8/26/18", "9/09/18", "9/21/18", "9/25/18", "9/31/18"]
-	]
+	"headers" => ["Study ID", "DAG", "Randomization Group", "Randomization/Surgery Date", "Call 1 approx. date due", "Call 2 approx. date due", "Call 3 approx. date due", "Site referral"],
+	"content" => []
 ];
-
-# get PT Scheduling table data from db
-$params = [
-	"project_id" => $dash->pid,
-	"return_format" => 'array',
-	"filterLogic" => "([baseline_arm_1][qtk_physical_therapy] <> '1') AND ([baseline_arm_1][qtk_physical_therapy] <> '0') AND ([enrollment_arm_1][pati_study_status]<>'0') AND (([enrollment_arm_1][randgroup]='2') OR (([enrollment_arm_1][randgroup]='1') AND ([enrollment_arm_1][pati_x15] <> '')))",
-	"exportDataAccessGroups" => true
-];
-$records = \REDCap::getData($params);
 foreach ($records as $i => $record) {
-	$rgroup = $record[$dash->enrollmentEID]['randgroup'];
-	$row = [];
-	$row[0] = $record[$dash->enrollmentEID]['study_id'];
-	$row[1] = $record[$dash->enrollmentEID]['pati_6'];
-	# print with label where possible, if not, print actual value
-	$row[2] = $rgroup == '1' ? 'Operative (1)' : ($rgroup == 2 ? 'Non-operative (2)' : $rgroup);
-	$row[3] = $rgroup == '1' ? $record[$dash->enrollmentEID]["pati_x15"] : ($rgroup == 2 ? $record[$dash->enrollmentEID]["date"] : '');
-	$row[4] = $rgroup == '1' ? $record[$dash->baselineEID]["qtk_call_due_pt_2"] : ($rgroup == 2 ? $record[$dash->baselineEID]["qtk_call_due_pt"] : '');
-	$row[5] = $record[$dash->baselineEID]["qtk_call_due_2"];
-	$row[6] = $record[$dash->baselineEID]["qtk_call_due_3"];
-	$row[7] = $record[$dash->baselineEID]["qtk_call_due_4"];
-	
-	$table['content'][] = $row;
+	$enrollment = $record[$dash->enrollmentEID];
+	$baseline = $record[$dash->baselineEID];
+	if ($baseline['qtk_physical_therapy'] <> '1' and
+	$baseline['qtk_physical_therapy'] <> '0' and
+	$enrollment['pati_study_status'] <> '0' and
+	($enrollment['randgroup'] == '2' or ($enrollment['randgroup'] == '1' and $enrollment['pati_x15'] <> ''))) {
+		$rgroup = $record[$dash->enrollmentEID]['randgroup'];
+		$row = [];
+		$row[0] = $record[$dash->enrollmentEID]['study_id'];
+		$row[1] = $record[$dash->enrollmentEID]['pati_6'];
+		# print with label where possible, if not, print actual value
+		// $row[2] = $rgroup == '1' ? 'Operative (1)' : ($rgroup == 2 ? 'Non-operative (2)' : $rgroup);
+		$row[2] = $this->labelizeValue("randgroup", $rgroup);
+		$row[3] = $rgroup == '1' ? $record[$dash->enrollmentEID]["pati_x15"] : ($rgroup == 2 ? $record[$dash->enrollmentEID]["date"] : '');
+		$row[4] = $rgroup == '1' ? $record[$dash->baselineEID]["qtk_call_due_pt_2"] : ($rgroup == 2 ? $record[$dash->baselineEID]["qtk_call_due_pt"] : '');
+		$row[5] = $record[$dash->baselineEID]["qtk_call_due_2"];
+		$row[6] = $record[$dash->baselineEID]["qtk_call_due_3"];
+		$row[7] = $record[$dash->baselineEID]["qtk_call_due_4"];
+		
+		$table['content'][] = $row;
+	}
 }
 
 # add table data to $content ($content will get output to user)
@@ -48,105 +52,103 @@ $content .= $dash->makeDataTable($table);
 $table = [
 	"title" => "Surgery Scheduling",
 	"titleClass" => "redHeader",
-	"headers" => ["Study ID", "DAG", "Randomization Date", "Surgery Scheduled Date", "Surgery Scheduling Notes"],
-	"content" => [
-		// # mockup data
-		// ["Knoxville Orthopedic Clinic", "4-3285-119", "9/31/18", "Piped from patient study info overview"],
-		// ["UTSW", "10-2284-23", "11/14/18", "Piped from patient study info overview"]
-	]
+	"headers" => ["Study ID", "DAG", "Randomization date:", "Surgery Scheduled Date", "Surgery Scheduling Notes"],
+	"content" => []
 ];
-$params = [
-	"project_id" => $dash->pid,
-	"return_format" => 'array',
-	"filterLogic" => "([enrollment_arm_1][randgroup] = '1') AND ([enrollment_arm_1][pati_x15] = '') AND ([enrollment_arm_1][pati_study_status] != '0')",
-	"exportDataAccessGroups" => true
-];
-$records = \REDCap::getData($params);
 foreach ($records as $i => $record) {
-	$row = [];
-	# study id
-	$row[0] = $record[$dash->enrollmentEID]['study_id'];
-	# dag
-	$row[1] = $record[$dash->enrollmentEID]['pati_6'];
-	# randomization date
-	$row[2] = $record[$dash->enrollmentEID]["date"];
-	# surgery scheduled date
-	$row[3] = $record[$dash->enrollmentEID]["pati_x15"];
-	# surgery scheduling notes
-	$row[4] = $record[$dash->enrollmentEID]["pati_surgical_sched_notes"];
-	
-	$table['content'][] = $row;
+	$edata = $record[$dash->enrollmentEID];
+	if ($edata['randgroup'] == '1' and $edata['pati_x15'] == '' and $edata['pati_study_status'] <> '0') {
+		$row = [];
+		$row[0] = $record[$dash->enrollmentEID]['study_id'];
+		$row[1] = $record[$dash->enrollmentEID]['pati_6'];
+		$row[2] = $record[$dash->enrollmentEID]["date"];
+		$row[3] = $record[$dash->enrollmentEID]["pati_16"];
+		$row[4] = $record[$dash->enrollmentEID]["pati_surgical_sched_notes"];
+		
+		$table['content'][] = $row;
+	}
 }
 $content .= $dash->makeDataTable($table);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// # add Delay of treatment deviations (upcoming) table
 $table = [
-	// "title" => "Delay of treatment deviations (upcoming)",
 	"title" => "Operative to Non-operative Potential Crossovers",
 	"titleClass" => "blueHeader",
-	"headers" => ["Study ID", "DAG", "Actual Surgery Date", "3m PT response", "6m PT response", "12m PT response"],
-	"content" => [
-		// ["4-3285-119", "operative", "10/30/18", "10/22/18"],
-		// ["10-2284-23", "non-operative", "10/30/18", ""]
-	]
+	"headers" => ["Study ID", "DAG", "Actual Surgery Date", "3m PT tx response", "6m PT tx response", "12m PT tx response"],
+	"content" => []
 ];
-$params = [
-	"project_id" => $dash->pid,
-	"return_format" => 'array',
-	"filterLogic" => "([3months_arm_1][sxu_b1] <> '1') AND ([6months_arm_1][sxu_b1] <> '1') AND ([12months_arm_1][sxu_b1] <> '1') AND ([enrollment_arm_1][pati_x15] = '') AND ([enrollment_arm_1][randgroup] = '1') AND ([3months_arm_1][tx_a7_fu] = '1' OR [6months_arm_1][tx_a7_fu] = '1' OR [12months_arm_1][tx_a7_fu] = '1' OR [enrollment_arm_1][pati_x16] <> '')",
-	"exportDataAccessGroups" => true
-];
-$records = \REDCap::getData($params);
 foreach ($records as $i => $record) {
-	$row = [];
-	$row[0] = $record[$dash->enrollmentEID]['study_id'];
-	$row[1] = $record[$dash->enrollmentEID]['pati_6'];
-	$row[2] = $record[$dash->enrollmentEID]["date"];
-	
-	$m3 = $record[81]["tx_a7_fu"];
-	$m6 = $record[82]["tx_a7_fu"];
-	$m12 = $record[83]["tx_a7_fu"];
-	
-	# if $m3 is empty, print empty, otherwise print label and value "Label (val)"
-	$row[3] = $m3 == '1' ? "Yes (1)" : ($m3 == '0' ? "No (0)" : $m3);
-	$row[4] = $m6 == '1' ? "Yes (1)" : ($m6 == '0' ? "No (0)" : $m6);
-	$row[5] = $m12 == '1' ? "Yes (1)" : ($m12 == '0' ? "No (0)" : $m12);
-	$table['content'][] = $row;
+	$edata = $record[$dash->enrollmentEID];
+	$m3data = $record[$dash->m3EID];
+	$m6data = $record[$dash->m6EID];
+	$m12data = $record[$dash->m12EID];
+	if (
+	($m3data['sxu_b1'] <> '1') and
+	($m6data['sxu_b1'] <> '1') and
+	($m12data['sxu_b1'] <> '1') and
+	($edata['pati_x15'] == '') and
+	($edata['randgroup'] == '1')
+	) {
+		$row = [];
+		$row[0] = $record[$dash->enrollmentEID]['study_id'];
+		$row[1] = $record[$dash->enrollmentEID]['pati_6'];
+		$row[2] = $record[$dash->enrollmentEID]["date"];
+		
+		$m3 = $record[$dash->m3EID]["tx_a7_fu"];
+		$m6 = $record[$dash->m6EID]["tx_a7_fu"];
+		$m12 = $record[$dash->m12EID]["tx_a7_fu"];
+		
+		# if $m3 is empty, print empty, otherwise print label and value "Label (val)"
+		$row[3] = $m3 == '1' ? "Yes (1)" : ($m3 == '0' ? "No (0)" : $m3);
+		$row[4] = $m6 == '1' ? "Yes (1)" : ($m6 == '0' ? "No (0)" : $m6);
+		$row[5] = $m12 == '1' ? "Yes (1)" : ($m12 == '0' ? "No (0)" : $m12);
+		$table['content'][] = $row;
+	}
 }
 $content .= $dash->makeDataTable($table);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// # add Delay of treatment deviations (need recorded) table
 $table = [
-	// "title" => "Delay of treatment deviations (need recorded)",
 	"title" => "Non-operative to Operative Potential Crossovers",
 	"titleClass" => "redHeader",
 	"headers" => ["Study ID", "DAG", "Actual Surgery Date", "3m sxu response", "6m sxu response", "12m sxu response"],
 	"content" => []
 ];
-$params = [
-	"project_id" => $dash->pid,
-	"return_format" => 'array',
-	"filterLogic" => "([3months_arm_1][sxu_b1] = '1' OR [6months_arm_1][sxu_b1] = '1' OR [12months_arm_1][sxu_b1] = '1' OR [enrollment_arm_1][pati_x15] <> '' OR [enrollment_arm_1][pati_16] <> '') AND ([enrollment_arm_1][randgroup] = '2')",
-	"exportDataAccessGroups" => true
-];
-$records = \REDCap::getData($params);
+// $params = [
+	// "project_id" => $dash->pid,
+	// "return_format" => 'array',
+	// "filterLogic" => "([3months_arm_1][sxu_b1] = '1' OR [6months_arm_1][sxu_b1] = '1' OR [12months_arm_1][sxu_b1] = '1' OR [enrollment_arm_1][pati_x15] <> '' OR [enrollment_arm_1][pati_16] <> '') AND ([enrollment_arm_1][randgroup] = '2')",
+	// "exportDataAccessGroups" => true
+// ];
+// $records = \REDCap::getData($params);
 foreach ($records as $i => $record) {
-	$row = [];
-	$row[0] = $record[$dash->enrollmentEID]['study_id'];
-	$row[1] = $record[$dash->enrollmentEID]['pati_6'];
-	$row[2] = $record[$dash->enrollmentEID]['pati_x15'];
-	
-	$m3 = $record[81]["sxu_b1"];
-	$m6 = $record[82]["sxu_b1"];
-	$m12 = $record[83]["sxu_b1"];
-	
-	# if $m3 is empty, print empty, otherwise print label and value "Label (val)"
-	$row[3] = $m3 == '1' ? "Yes (1)" : ($m3 == '0' ? "No (0)" : $m3);
-	$row[4] = $m6 == '1' ? "Yes (1)" : ($m6 == '0' ? "No (0)" : $m6);
-	$row[5] = $m12 == '1' ? "Yes (1)" : ($m12 == '0' ? "No (0)" : $m12);
-	$table['content'][] = $row;
+	$m3data = $record[$dash->m3EID];
+	$m6data = $record[$dash->m6EID];
+	$m12data = $record[$dash->m12EID];
+	$edata = $record[$dash->enrollmentEID];
+	if (
+	$m3data['sxu_b1'] == '1' or
+	$m6data['sxu_b1'] == '1' or
+	$m12data['sxu_b1'] == '1' or
+	$edata['pati_x15'] <> '' or
+	$edata['pati_16'] <> '' and
+	$edata['randgroup'] == '2'
+	) {
+		$row = [];
+		$row[0] = $record[$dash->enrollmentEID]['study_id'];
+		$row[1] = $record[$dash->enrollmentEID]['pati_6'];
+		$row[2] = $record[$dash->enrollmentEID]['pati_x15'];
+		
+		$m3 = $record[$dash->m3EID]["sxu_b1"];
+		$m6 = $record[$dash->m6EID]["sxu_b1"];
+		$m12 = $record[$dash->m12EID]["sxu_b1"];
+		
+		# if $m3 is empty, print empty, otherwise print label and value "Label (val)"
+		$row[3] = $m3 == '1' ? "Yes (1)" : ($m3 == '0' ? "No (0)" : $m3);
+		$row[4] = $m6 == '1' ? "Yes (1)" : ($m6 == '0' ? "No (0)" : $m6);
+		$row[5] = $m12 == '1' ? "Yes (1)" : ($m12 == '0' ? "No (0)" : $m12);
+		$table['content'][] = $row;
+	}
 }
 $content .= $dash->makeDataTable($table);
 $content .= "<br />";
