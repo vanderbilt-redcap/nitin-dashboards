28fded9f43afee92f989e021622edf718007bd6c
Carl Reed <carl.w.reed@vumc.org>
2019-02-14 11:19:27 -0600

    fixed enrollment ID record links for production and test servers

--- config.php
+++ config.php
@@ -15,9 +15,9 @@ if(!defined("ENVIRONMENT")) {
 			// define("SUBJECT_PID", N/A);
 		}
 		
-		define("SUBJECT_RECORD_URL", APP_PATH_WEBROOT_FULL . "DataEntry/record_home.php?pid=" . SUBJECT_PID . "&id=");
-		define("SCREENING_RECORD_URL", APP_PATH_WEBROOT_FULL . "DataEntry/index.php?pid=" . SCREENING_PID . "&page=screening_log&id=");
-		define("IMAGING_RECORD_URL", APP_PATH_WEBROOT_FULL . "DataEntry/record_home.php?pid=" . IMAGING_PID . "&id=");
+		define("SUBJECT_RECORD_URL", APP_PATH_WEBROOT_FULL . substr(APP_PATH_WEBROOT, 1) . "DataEntry/record_home.php?pid=" . SUBJECT_PID . "&id=");
+		define("SCREENING_RECORD_URL", APP_PATH_WEBROOT_FULL . substr(APP_PATH_WEBROOT, 1) . "DataEntry/index.php?pid=" . SCREENING_PID . "&page=screening_log&id=");
+		define("IMAGING_RECORD_URL", APP_PATH_WEBROOT_FULL . substr(APP_PATH_WEBROOT, 1) . "DataEntry/record_home.php?pid=" . IMAGING_PID . "&id=");
 	} else {
 		define("ENVIRONMENT", "DEV");
 		// define("SUBJECT_PID", 13);		// @able
