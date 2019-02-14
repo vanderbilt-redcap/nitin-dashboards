79627a2f2c7d7884f4b9e816fde7ed13b6e2fa10
Carl Reed <carl.w.reed@vumc.org>
2019-02-14 12:54:30 -0600

    added a spinning loader that displays between screen changes, screen scrolls to top on ajax complete, added some margin to bottom of last div on each screen, corrected enrollment ID links

--- js/base.js
+++ js/base.js
@@ -12,6 +12,11 @@ $(function() {
 		$("nav div").removeClass('selected');
 		$(this).addClass('selected');
 		
+		// fadeOut content div and fadeIn loader before firing ajax request
+		$("#content").fadeOut(150, function() {
+			$("#loader").fadeIn(100);
+		});
+		
 		// ajax requested screen
 		// var url = window.location.origin + "/plugins/nitin/index.php";
 		$.ajax({
@@ -24,6 +29,10 @@ $(function() {
 					paging: false,
 					info: false,
 					searching: false
+				});
+				document.body.scrollTop = document.documentElement.scrollTop = 0;
+				$("#loader").fadeOut(100, function() {
+					$("#content").fadeIn(150);
 				});
 			},
 			fail : function(data) {
