4c2c05863ab4ce9c5100c35b441fa6a41b8860ce
Carl Reed <carl.w.reed@vumc.org>
2019-02-14 10:49:21 -0600

    updated logic to target repeat_instances for table Follow-Up calls...

--- html/patient_followup.php
+++ html/patient_followup.php
@@ -127,31 +127,39 @@ $table = [
 	"headers" => ["Study ID", "DAG", "Event", "Instance:", "Information to be validated", "Resolution notes", "Date issue(s) discovered", "Contact 1 Date", "Contact 2 Date", "Contact 3 Date", "Contact 4 Date", "Contact 5 Date"],
 	"content" => []
 ];
+
+# diagnostics
+// $out = [];
+// $out['$datatype'] = gettype($records[14]['repeat_instances'][82]['data_collectionvalidation'][1]);
+// $out['baselineEID'] = $dash->baselineEID;
+// $out['record14'] = $records[14];
+// exit("<pre>" . print_r($out, true) . "</pre>");
+
 foreach ($records as $i => $record) {
 	$edata = $record[$dash->enrollmentEID];
-	// foreach ($record as $eid => $data) {
-	foreach ($record['repeat_instances'][$dash->baselineEID]['data_collectionvalidation'] as $eid => $data) {
-		if (
-			
-			$data['dval_pat_contact_needed'] == '1' and
-			($data['dval_res_pat'] == '2' or $data['dval_res_pat'] == '') and
-			$edata['pati_study_status'] <> '0'
-		) {
-			$row = [];
-			$row[0] = "<a href = \"" . $dash->recordHome . "$i\">" . $edata['enrollment_id'] . "</a> " . $edata['study_id'];
-			$row[1] = $edata['pati_6'];
-			$row[2] = $dash->projEvents[$eid];
-			$row[3] = ''; // how to get redcap_repeat_instance??
-			$row[4] = $data['dval_specify_patient'];
-			$row[5] = $data['dval_res_notes'];
-			$row[6] = $data['dval_issue_disc_date'];
-			$row[7] = $data['dval_contact_date_1'];
-			$row[8] = $data['dval_contact_date_2'];
-			$row[9] = $data['dval_contact_date_3'];
-			$row[10] = $data['dval_contact_date_4'];
-			$row[11] = $data['dval_contact_date_5'];
-			
-			$table['content'][] = $row;
+	foreach ($record['repeat_instances'] as $eid => $eventData) {
+		foreach ($eventData['data_collectionvalidation'] as $repeatInstanceIndex => $data) {
+			if (
+				$data['dval_pat_contact_needed'] == '1' and
+				($data['dval_res_pat'] == '2' or $data['dval_res_pat'] == '') and
+				$data['pati_study_status'] <> '0'
+			) {
+				$row = [];
+				$row[0] = "<a href = \"" . $dash->recordHome . "$i\">" . $edata['enrollment_id'] . "</a> " . $edata['study_id'];
+				$row[1] = $edata['pati_6'];
+				$row[2] = $dash->projEvents[$eid];
+				$row[3] = ''; // how to get redcap_repeat_instance??
+				$row[4] = $data['dval_specify_patient'];
+				$row[5] = $data['dval_res_notes'];
+				$row[6] = $data['dval_issue_disc_date'];
+				$row[7] = $data['dval_contact_date_1'];
+				$row[8] = $data['dval_contact_date_2'];
+				$row[9] = $data['dval_contact_date_3'];
+				$row[10] = $data['dval_contact_date_4'];
+				$row[11] = $data['dval_contact_date_5'];
+				
+				$table['content'][] = $row;
+			}
 		}
 	}
 }
