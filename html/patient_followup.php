6f918546986fec7b6bf35ac9f197f4960272a75f
Carl Reed <carl.w.reed@vumc.org>
2019-03-18 12:09:53 -0500

    logic update

--- html/patient_followup.php
+++ html/patient_followup.php
@@ -64,15 +64,22 @@ foreach ($records as $i => $record) {
 		if(
 			// logic from "Physical therapy diary checks" report
 			// ([enrollment_arm_1][pati_14] = "" OR (([1month_arm_1][pttk_diary_check] <> "1") AND ([1month_arm_1][pttk_pt_report_sent] <> "")) OR (([3months_arm_1][qtk_questionnaire_sent_2] = "") AND ([3months_arm_1][qtk_questionnaire_sent] = "1")) OR (([6months_arm_1][qtk_questionnaire_sent_2] = "") AND ([6months_arm_1][qtk_questionnaire_sent] = "1")))
-			// ([pttk_diary_check] = "" OR [qtk_questionnaire_received_2] = "") AND ([qtk_questionnaire_sent] = "1") AND ([patc] <> "1")
 			
 			($edata['pati_14'] == "" or 
-			($m1data['pttk_diary_check'] <> "1") and 
+			(($m1data['pttk_diary_check'] <> "1") and 
 			($m1data['pttk_pt_report_sent'] <> "")) or 
 			(($m3data['qtk_questionnaire_sent_2'] == "") and 
 			($m3data['qtk_questionnaire_sent'] == "1")) or 
 			(($m6data['qtk_questionnaire_sent_2'] == "") and 
-			($m6data['qtk_questionnaire_sent'] == "1"))
+			($m6data['qtk_questionnaire_sent'] == "1")))
+			
+			// ($edata['pati_14'] == "" or 
+			// ($m1data['pttk_diary_check'] <> "1") and 
+			// ($m1data['pttk_pt_report_sent'] <> "")) or 
+			// (($m3data['qtk_questionnaire_sent_2'] == "") and 
+			// ($m3data['qtk_questionnaire_sent'] == "1")) or 
+			// (($m6data['qtk_questionnaire_sent_2'] == "") and 
+			// ($m6data['qtk_questionnaire_sent'] == "1"))
 			
 			// ($data['pttk_diary_check'] == "" or
 			// $data['qtk_questionnaire_received_2'] == "") and
