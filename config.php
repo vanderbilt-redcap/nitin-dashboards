b094c52fac3cfbe149b6ad5fa4011583b88656b0
moorejr5 <james.r.moore@vanderbilt.edu>
2020-03-10 12:53:29 -0500

    Updating for new Victrlib Env class.

--- config.php
+++ config.php
@@ -1,16 +1,21 @@
 <?php
-if(!defined("ENVIRONMENT")) {
-	if (is_file('/app001/victrcore/lib/Victr/Env.php')) include_once('/app001/victrcore/lib/Victr/Env.php');
-	if (class_exists("Victr_Env")) {
-		$envConf = Victr_Env::getEnvConf();
+use Vanderbilt\Victrlib\Env;
 
-		if ($envConf[Victr_Env::ENV_CURRENT] === Victr_Env::ENV_PROD) {
+error_reporting(E_ALL & ~E_WARNING);
+
+if(!defined("ENVIRONMENT")) {
+    if (is_file('/app001/www/redcap/plugins/victrlib/src/Env.php'))
+        include_once('/app001/www/redcap/plugins/victrlib/src/Env.php');
+	if (class_exists("\\Vanderbilt\\Victrlib\\Env")) {
+		//$envConf = Victr_Env::getEnvConf();
+
+		if (Env::isProd()) {
 			define("ENVIRONMENT", "PROD");
 			define("SCREENING_PID", 63382);	// real project on prod");
 			define("SUBJECT_PID", 63383);	// real project on prod");
 			define("IMAGING_PID", 63384);	// real project on prod");
 			// define("SUBJECT_PID", 73340);	// test project on prod");
-		} elseif ($envConf[Victr_Env::ENV_CURRENT] === Victr_Env::ENV_DEV) {
+		} elseif (Env::isStaging()) {
 			define("ENVIRONMENT", "TEST");
 			define("SCREENING_PID", 63382);	// real project on prod");
 			define("SUBJECT_PID", 63383);	// real project on prod");
@@ -29,9 +34,9 @@ if(!defined("ENVIRONMENT")) {
 		// define("SUBJECT_PID", 13);		// @able
 		// define("IMAGING_PID", 14);		// @able
 		// define("SCREENING_PID", 15);	// @able
-		define("SUBJECT_PID", 21);
-		define("SCREENING_PID", 22);
-		define("IMAGING_PID", 23);
+		define("SUBJECT_PID", 123);
+		define("SCREENING_PID", 122);
+		define("IMAGING_PID", 124);
 		
 		define("SUBJECT_RECORD_URL", substr(APP_PATH_WEBROOT_FULL, 0, -8) . APP_PATH_WEBROOT . "DataEntry/record_home.php?pid=" . SUBJECT_PID . "&id=");
 		define("SCREENING_RECORD_URL", substr(APP_PATH_WEBROOT_FULL, 0, -8) . APP_PATH_WEBROOT . "DataEntry/index.php?pid=" . SCREENING_PID . "&page=screening_log&id=");
