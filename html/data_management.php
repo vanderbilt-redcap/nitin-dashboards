de58c75bcc04b2beadd77f65788e0a9b3436ddf0
Carl Reed <carlreedw@gmail.com>
2020-04-24 12:45:32 -0500

    updated data management tab tables

--- html/data_management.php
+++ html/data_management.php
@@ -5,59 +5,78 @@ $content = "";
 $params = [
 	"project_id" => $dash->pid,
 	"return_format" => 'array',
-	// "fields" => [
-		// 'dval_contact_date_1',
-		// 'dval_specify_research',
-		// 'pati_x15',
-		// 'qtk_date_received',
-		// 'de_comp_initials_crf02b',
-		// 'sdoc_initial_due',
-		// 'date',
-		// 'de_2_comp_initials_crf04_b',
-		// 'de_comp_initials_crf03_3m',
-		// 'de_comp_initials_crf03_12m',
-		// 'qtk_date_received_2',
-		// 'randgroup',
-		// 'de_comp_initials_crf02a',
-		// 'pati_x15',
-		// 'sdoc_surgical_due',
-		// 'study_id',
-		// 'css',
-		// 'de_2_comp_initials_crf03_bl',
-		// 'de_comp_initials_crf00',
-		// 'dval_contact_date_5',
-		// 'dval_res_notes',
-		// 'de_comp_initials_crf04_b',
-		// 'de_comp_initials_crf03_bl',
-		// 'qtk_physical_therapy',
-		// 'pati_6',
-		// 'dval_res_ra',
-		// 'date',
-		// 'de_2_comp_initials_crf05',
-		// 'de_comp_initials_crf05',
-		// 'dval_contact_date_2',
-		// 'de_2_comp_initials_crf03_6m',
-		// 'dval_action_needed',
-		// 'de_comp_initials_crf03_6m',
-		// 'dval_action_needed',
-		// 'pati_surgical_sched_notes',
-		// 'de_2_comp_initials_crf00',
-		// 'de_2_comp_initials_crf02b',
-		// 'de_2_comp_initials_crf04_a',
-		// 'de_2_comp_initials_crf03_12m',
-		// 'dval_contact_date_3',
-		// 'qtk_pdv_effective_date',
-		// 'dval_contact_date_4',
-		// 'dval_res_ra',
-		// 'de_comp_initials_crf04_a',
-		// 'dval_issue_disc_date',
-		// 'sdoc_vumc_cert_2',
-		// 'sdoc_vumc_cert',
-		// 'de_2_comp_initials_crf02a',
-		// 'de_2_comp_initials_crf03_3m',
-		// 'enrollment_id',
-		// 'data_collectionvalidation'
-	// ],
+	"fields" => [
+		'sdoc_initial_due',
+		'sdoc_vumc_cert',
+		'enrollment_id',
+		'study_id',
+		'pati_6',
+		'randgroup',
+		'date',
+		'sdoc_vumc_cert_2',
+		'surgery_report_form_crf05_complete',
+		'pati_x15',
+		'sdoc_surgical_due',
+		'recruit_md_name',
+		'sdoc_sx_note',
+		'sdoc_sxr',
+		'dem_crf_05',
+		'dval_action_needed',
+		'dval_res_ra',
+		'dval_specify_research',
+		'dval_res_notes',
+		'dval_issue_disc_date',
+		'dval_contact_date_1',
+		'dval_contact_date_2',
+		'dval_contact_date_3',
+		'dval_contact_date_4',
+		'dval_contact_date_5',
+		'qtk_pdv_effective_date',
+		'qtk_physical_therapy',
+		'pati_surgical_sched_notes',
+		'de_comp_crf00',
+		'de_comp_crf02a',
+		'de_comp_crf02b',
+		'de_comp_crf03_bl',
+		'de_comp_crf05',
+		'de_comp_crf03_3m',
+		'de_comp_crf04_a',
+		'de_comp_crf03_6m',
+		'de_comp_crf04_b',
+		'de_comp_crf03_12m',
+		'de_2_comp_crf00',
+		'de_2_comp_crf02a',
+		'de_2_comp_crf02b',
+		'de_2_comp_crf03_bl',
+		'de_2_comp_crf05',
+		'de_2_comp_crf03_3m',
+		'de_2_comp_crf04_a',
+		'de_2_comp_crf03_6m',
+		'de_2_comp_crf04_b',
+		'de_2_comp_crf03_12m',
+		'de_comp_initials_crf00',
+		'de_comp_initials_crf02a',
+		'de_comp_initials_crf02b',
+		'de_comp_initials_crf03_bl',
+		'de_comp_initials_crf05',
+		'de_comp_initials_crf03_3m',
+		'de_comp_initials_crf04_a',
+		'de_comp_initials_crf03_6m',
+		'de_comp_initials_crf04_b',
+		'de_comp_initials_crf03_12m',
+		'de_2_comp_initials_crf00',
+		'de_2_comp_initials_crf02a',
+		'de_2_comp_initials_crf02b',
+		'de_2_comp_initials_crf03_bl',
+		'de_2_comp_initials_crf05',
+		'de_2_comp_initials_crf03_3m',
+		'de_2_comp_initials_crf04_a',
+		'de_2_comp_initials_crf03_6m',
+		'de_2_comp_initials_crf04_b',
+		'de_2_comp_initials_crf03_12m',
+		'qtk_date_received',
+		'qtk_date_received_2'
+	],
 	"exportDataAccessGroups" => true
 ];
 $records = \REDCap::getData($params);
@@ -67,7 +86,11 @@ $table = [
 	"title" => "Enrollment Document Collection",
 	"titleClass" => "blueHeader",
 	"headers" => ["Study ID", "DAG", "Randomization Group", "Randomization date:", "These documents MUST be uploaded BY:", "Days since date due:"],
-	"content" => []
+	"content" => [],
+	"attributes" => [
+		"order-col" => 5,
+		"order-direction" => "desc"
+	]
 ];
 foreach ($records as $i => $record) {
 	$edata = $record[$dash->enrollmentEID];
@@ -94,23 +117,58 @@ $content .= $dash->makeDataTable($table);
 $table = [
 	"title" => "Surgical Document Collection",
 	"titleClass" => "redHeader",
-	"headers" => ["Study ID", "DAG", "Randomization date", "Actual Surgery Date:", "These documents MUST be uploaded BY:", "Days since date due:"],
-	"content" => []
+	"headers" => [
+		"Study ID",
+		"DAG",
+		"Randomization date",
+		"Actual Surgery Date:",
+		"These documents MUST be uploaded BY:",
+		"Recruiting Physician",
+		"Surgical Note (uploaded for ALL patients w/surgery",
+		"Surgery Report Form (if paper)",
+		"Surgery Report Form Data Entry Method",
+		"Surgery Report Form Completion Status (if direct data entry)",
+		"Days since date due:"
+	],
+	"content" => [],
+	"attributes" => [
+		"order-col" => 10,
+		"order-direction" => "desc"
+	]
 ];
+
 foreach ($records as $i => $record) {
 	$edata = $record[$dash->enrollmentEID];
-	if ($edata['sdoc_vumc_cert_2'] <> '1' and $edata['randgroup'] == '1' and $edata['pati_x15'] <> '') {
+	$otherdata = $record[$dash->otherEID];
+	
+	if (
+		($edata['sdoc_vumc_cert_2'] <> '1' or 
+		$otherdata['surgery_report_form_crf05_complete'] <> '2') and 
+		$edata['pati_x15'] <> ''
+	) {
 		$row = [];
 		$row[0] = "<a href = \"" . $dash->recordHome . "$i\">" . $edata['enrollment_id'] . "</a> " . $edata['study_id'];
 		$row[1] = $edata['pati_6'];
 		$row[2] = $edata['date'];
 		$row[3] = $edata['pati_x15'];
 		$row[4] = $edata['sdoc_surgical_due'];
+		$row[5] = $edata['recruit_md_name'];
 		
-		if (empty($row[4]) or $row[4] >= $today) {
-			$row[5] = 0;
-		} else {
-			$row[5] = date_diff(date_create($row[4]), date_create($today))->format("%a");
+		// surg doc note field
+		$row[6] = "";
+		if (!empty($edata['sdoc_sx_note'])) {
+			$hash = \Files::docIdHash($edata['sdoc_sx_note']);
+			$url = APP_PATH_WEBROOT . "/DataEntry/file_download.php?pid=" . $dash->pid . "&record=$i&event_id=" . $dash->enrollmentEID . "&instance=1&field_name=sdoc_sx_note&id=" . $edata['sdoc_sx_note'] . "&doc_id_hash=$hash";
+			$row[6] = "<button onclick='window.open(\"$url\",\"_blank\");'>Download</button>";
+		}
+		
+		$row[7] = $dash->labelizeValue('sdoc_sxr', $otherdata['sdoc_sxr']);
+		$row[8] = $dash->labelizeValue('dem_crf_05', $otherdata['dem_crf_05']);
+		$row[9] = $dash->labelizeValue('surgery_report_form_crf05_complete', $otherdata['surgery_report_form_crf05_complete']);
+		$row[10] = 0;
+		
+		if ($today < $edata['sdoc_surgical_due']) {
+			$row[10] = date_diff(date_create($edata['sdoc_surgical_due']), date_create($today))->format("%a");
 		}
 		
 		$table['content'][] = $row;
@@ -123,7 +181,11 @@ $table = [
 	"title" => "Data Validation (Sites/Personnel)",
 	"titleClass" => "blueHeader",
 	"headers" => ["Study ID", "DAG", "Event", "Instance", "Information to be validated", "Resolution notes", "Date issue(s) discovered", "Contact 1 Date", "Contact 2 Date", "Contact 3 Date", "Contact 4 Date", "Contact 5 Date", "Days since last contact attempt:"],
-	"content" => []
+	"content" => [],
+	"attributes" => [
+		"order-col" => 12,
+		"order-direction" => "desc"
+	]
 ];
 foreach ($records as $i => $record) {
 	$edata = $record[$dash->enrollmentEID];
@@ -163,7 +225,11 @@ $table = [
 	"title" => "Delay of Treatment Protocol Deviations (to be recorded)",
 	"titleClass" => "redHeader",
 	"headers" => ["Study ID", "DAG", "Randomization group", "Date delay of treatment effective:", "Surgery scheduling status/notes: (if applicable:"],
-	"content" => []
+	"content" => [],
+	"attributes" => [
+		"order-col" => 3,
+		"order-direction" => "asc"
+	]
 ];
 $day0 = date('Y-m-d');	# set target date to be 30 days from now
 foreach ($records as $i => $record) {
@@ -191,7 +257,11 @@ $table = [
 	"title" => "Delay of Treatment Protocol Deviations (approaching)",
 	"titleClass" => "blueHeader",
 	"headers" => ["Study ID", "DAG", "Randomization Group", "Date delay of treatment effective:", "Surgery scheduling status/notes: (if applicable:"],
-	"content" => []
+	"content" => [],
+	"attributes" => [
+		"order-col" => 3,
+		"order-direction" => "asc"
+	]
 ];
 $day0 = date('Y-m-d');
 $day30 = date('Y-m-d', strtotime('+30 days'));	# set target date to be 30 days from now
@@ -216,7 +286,7 @@ foreach ($records as $i => $record) {
 }
 $content .= $dash->makeDataTable($table);
 
-// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 $table = [
 	"title" => "Double Data Entry",
 	"titleClass" => "redHeader",
@@ -235,83 +305,97 @@ foreach ($records as $i => $record) {
 	$m3data = $record[$dash->m3EID];
 	$m6data = $record[$dash->m6EID];
 	$m12data = $record[$dash->m12EID];
-	foreach ($record as $j => $data) {
+	$otherdata = $record[$dash->otherEID];
+	
+	if (
+		($otherdata['de_comp_crf00'] == "1" AND
+		$otherdata['de_2_comp_crf00'] == "") OR
+		($otherdata['de_comp_crf02a'] == "1" AND
+		$otherdata['de_2_comp_crf02a'] == "") OR
+		($otherdata['de_comp_crf02b'] == "1" AND
+		$otherdata['de_2_comp_crf02b'] == "") OR
+		($otherdata['de_comp_crf03_bl'] == "1" AND
+		$otherdata['de_2_comp_crf03_bl'] == "") OR
+		($otherdata['de_comp_crf05'] == "1" AND
+		$otherdata['de_2_comp_crf05'] == "") OR
+		($otherdata['de_comp_crf03_3m'] == "1" AND
+		$otherdata['de_2_comp_crf03_3m'] == "") OR
+		($otherdata['de_comp_crf04_a'] == "1" AND
+		$otherdata['de_2_comp_crf04_a'] == "") OR
+		($otherdata['de_comp_crf03_6m'] == "1" AND
+		$otherdata['de_2_comp_crf03_6m'] == "") OR
+		($otherdata['de_comp_crf04_b'] == "1" AND
+		$otherdata['de_2_comp_crf04_b'] == "") OR
+		($otherdata['de_comp_crf03_12m'] == "1" AND
+		$otherdata['de_2_comp_crf03_12m'] == "")
+	) {
 		$row = [];
 		$css = [];
-		$row[0] = $edata['study_id'];
-		$row[1] = $data["de_comp_initials_crf00"];
-		$row[2] = $data["de_2_comp_initials_crf00"];
+		$row[0] = "<a href = \"" . $dash->recordHome . "$i\">" . $edata['enrollment_id'] . "</a> " . $edata['study_id'];
+		$row[1] = $otherdata["de_comp_initials_crf00"];
+		$row[2] = $otherdata["de_2_comp_initials_crf00"];
 		if (!empty($row[1]) and empty($row[2])) {
 			$row[2] = $edata["date"];
 			$css[2] = 1;
 		}
-		$row[3] = $data["de_comp_initials_crf02a"];
-		$row[4] = $data["de_2_comp_initials_crf02a"];
+		$row[3] = $otherdata["de_comp_initials_crf02a"];
+		$row[4] = $otherdata["de_2_comp_initials_crf02a"];
 		if (!empty($row[3]) and empty($row[4])) {
 			$row[4] = $edata["date"];
 			$css[4] = 1;
 		}
-		$row[5] = $data["de_comp_initials_crf02b"];
-		$row[6] = $data["de_2_comp_initials_crf02b"];
+		$row[5] = $otherdata["de_comp_initials_crf02b"];
+		$row[6] = $otherdata["de_2_comp_initials_crf02b"];
 		if (!empty($row[5]) and empty($row[6])) {
 			$row[6] = $edata["date"];
 			$css[6] = 1;
 		}
-		$row[7] = $data["de_comp_initials_crf03_bl"];
-		$row[8] = $data["de_2_comp_initials_crf03_bl"];
+		$row[7] = $otherdata["de_comp_initials_crf03_bl"];
+		$row[8] = $otherdata["de_2_comp_initials_crf03_bl"];
 		if (!empty($row[7]) and empty($row[8])) {
 			$row[8] = $edata["date"];
 			$css[8] = 1;
 		}
-		$row[9] = $data["de_comp_initials_crf03_3m"];
-		$row[10] = $data["de_2_comp_initials_crf03_3m"];
+		$row[9] = $otherdata["de_comp_initials_crf03_3m"];
+		$row[10] = $otherdata["de_2_comp_initials_crf03_3m"];
 		if (!empty($row[9]) and empty($row[10])) {
 			$row[10] = $m3data["qtk_date_received"];
 			$css[10] = 1;
 		}
-		$row[11] = $data["de_comp_initials_crf04_a"];
-		$row[12] = $data["de_2_comp_initials_crf04_a"];
+		$row[11] = $otherdata["de_comp_initials_crf04_a"];
+		$row[12] = $otherdata["de_2_comp_initials_crf04_a"];
 		if (!empty($row[11]) and empty($row[12])) {
 			$row[12] = $m3data["qtk_date_received_2"];
 			$css[12] = 1;
 		}
-		$row[13] = $data["de_comp_initials_crf03_6m"];
-		$row[14] = $data["de_2_comp_initials_crf03_6m"];
+		$row[13] = $otherdata["de_comp_initials_crf03_6m"];
+		$row[14] = $otherdata["de_2_comp_initials_crf03_6m"];
 		if (!empty($row[13]) and empty($row[14])) {
 			$row[14] = $m6data["qtk_date_received"];
 			$css[14] = 1;
 		}
-		$row[15] = $data["de_comp_initials_crf04_b"];
-		$row[16] = $data["de_2_comp_initials_crf04_b"];
+		$row[15] = $otherdata["de_comp_initials_crf04_b"];
+		$row[16] = $otherdata["de_2_comp_initials_crf04_b"];
 		if (!empty($row[15]) and empty($row[16])) {
 			$row[16] = $m6data["qtk_date_received_2"];
 			$css[16] = 1;
 		}
-		$row[17] = $data["de_comp_initials_crf03_12m"];
-		$row[18] = $data["de_2_comp_initials_crf03_12m"];
+		$row[17] = $otherdata["de_comp_initials_crf03_12m"];
+		$row[18] = $otherdata["de_2_comp_initials_crf03_12m"];
 		if (!empty($row[17]) and empty($row[18])) {
 			$row[18] = $m12data["qtk_date_received"];
 			$css[18] = 1;
 		}
-		$row[19] = $data["de_comp_initials_crf05"];
-		$row[20] = $data["de_2_comp_initials_crf05"];
+		$row[19] = $otherdata["de_comp_initials_crf05"];
+		$row[20] = $otherdata["de_2_comp_initials_crf05"];
 		if (!empty($row[19]) and empty($row[20])) {
 			$row[20] = $edata["pati_x15"];
 			$css[20] = 1;
 		}
-		
-		$ignoreThisRow = true;
-		for ($i = 1; $i <= 20; $i++) {
-			if (!empty($row[$i])) {
-				$ignoreThisRow = false;
-				break;
-			}
-		}
-		if (!$ignoreThisRow) {
-			$table["css"][] = $css;
-			$table['content'][] = $row;
-		}
+		$table["css"][] = $css;
+		$table['content'][] = $row;
 	}
+	
 }
 $content .= $dash->makeDDETable($table);
 
